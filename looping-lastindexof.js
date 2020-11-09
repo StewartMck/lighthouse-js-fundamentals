3c7a371b7ecfd30b0f85e3037b8af8329e858217
Stewart McKinlay <stewart.mckinlay@gmail.com>
2020-11-09 03:17:54 +0000

    Looping - lastIndexOf exercise

---
+++ looping-lastindexof.js
@@ -0,0 +1,11 @@
+const lastIndexOf = function (array, index) {
+  return array.lastIndexOf(index);
+}
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1));
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2));
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3));
+console.log(lastIndexOf([5, 5, 5], 5));
+console.log(lastIndexOf([], 3));
+
+
